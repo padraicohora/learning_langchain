e725937c884aa44b6e80cf58d8d85168fdbbfb5e
pohora <pohora@qti.qualcomm.com>
2025-08-21 15:36:45 +0100

    completed chapter 1

--- qgenie_setup.py
+++ qgenie_setup.py
@@ -1,7 +1,6 @@
-from rich.pretty import pprint
 import os
 from dotenv import load_dotenv
-from qgenie import QGenieClient
+from qgenie import QGenieClient, ChatMessage
 
 load_dotenv()
 
@@ -9,6 +8,6 @@ qgenie_api_key = os.getenv("QGENIE_API_KEY")
 
 client = QGenieClient(endpoint="https://qgenie-chat.qualcomm.com", api_key=qgenie_api_key)
 
-models_response = client.get_available_models()
 
-pprint(models_response, expand_all=True)
+
+
